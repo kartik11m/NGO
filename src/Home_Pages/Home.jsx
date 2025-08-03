723e6c53122cc45fd22d0f9121d291b863d02337
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-04 00:39:14 +0530

    Using Layout for Home Page

---
+++ src/Home_Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero'
+import Layout from '../components/Layout'
+
+const Home = () => {
+  return (
+    <>
+        <Layout>
+            <Hero/>
+        </Layout>
+    </>
+  )
+}
+
+export default Home
