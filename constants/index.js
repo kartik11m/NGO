4d2c1942323d4d117b8b9ea2b4922e614ed43b62
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-03 17:38:48 +0530

    adding the content of the Navbar

---
+++ constants/index.js
@@ -0,0 +1,26 @@
+const navLinks = [
+ {
+	id: "home",
+	title: "Home",
+ },
+ {
+	id: "about",
+	title: "About Us",
+ },
+ {
+	id: "work",
+	title: "What We Do",
+ },
+ {
+    id: "media",
+    title:"Media"
+ },
+ {
+	id: "contact",
+	title: "Contact",
+ },
+];
+
+export{
+    navLinks,
+}
