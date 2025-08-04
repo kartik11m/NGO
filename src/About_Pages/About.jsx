831dd8ab06b1548a3e496908e3e25ad54c02ba1b
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-04 23:41:26 +0530

    About Layout Created

---
+++ src/About_Pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Section1 from './Section1'
+
+const About = () => {
+  return (
+    <Layout>
+        <Section1/>
+    </Layout>
+  )
+}
+
+export default About
