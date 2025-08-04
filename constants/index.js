01246789938e2ae88db18e356891fc793b0df822
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-05 03:16:08 +0530

    gridData was added

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,8 @@
+import 'remixicon/fonts/remixicon.css'
+
 const navLinks = [
  {
-	id: "home",
+	id: "/",
 	title: "Home",
  },
  {
@@ -21,6 +23,83 @@ const navLinks = [
  },
 ];
 
+const gridData = [
+	{
+		img: "/images/user.png",
+		name: "Leonard John Davies",
+		pos:"Cofounder, CEO",
+		face: "ri-facebook-circle-fill",
+		tweet:  "ri-twitter-fill",
+		linked: "ri-linkedin-box-fill"
+
+	},
+	{
+		img: "/images/user1.png",
+		name: "Francis Weber",
+		pos:"Vice Chairman",
+		face: "ri-facebook-circle-fill",
+		tweet:  "ri-twitter-fill",
+		linked: "ri-linkedin-box-fill"
+
+	},
+	{
+		img: "/images/user2.png",
+		name: "Kyla Obrien",
+		pos:"Head of Authority",
+		face: "ri-facebook-circle-fill",
+		tweet:  "ri-twitter-fill",
+		linked: "ri-linkedin-box-fill"
+
+	},
+	{
+		img: "/images/user3.png",
+		name: "Adrian Dixon",
+		pos:"Support Executive",
+		face: "ri-facebook-circle-fill",
+		tweet:  "ri-twitter-fill",
+		linked: "ri-linkedin-box-fill"
+
+	},
+	{
+		img: "/images/user4.png",
+		name: "Freddy Busby",
+		pos:"Project Manager",
+		face: "ri-facebook-circle-fill",
+		tweet:  "ri-twitter-fill",
+		linked: "ri-linkedin-box-fill"
+
+	},
+	{
+		img: "/images/user5.png",
+		name: "Dale Banks",
+		pos:"Accountatnt, Finance",
+		face: "ri-facebook-circle-fill",
+		tweet:  "ri-twitter-fill",
+		linked: "ri-linkedin-box-fill"
+
+	},
+	{
+		img: "/images/user6.png",
+		name: "Miriam Middleton",
+		pos:"Cofounder, CEO",
+		face: "ri-facebook-circle-fill",
+		tweet:  "ri-twitter-fill",
+		linked: "ri-linkedin-box-fill"
+
+	},
+	{
+		img: "/images/user7.png",
+		name: "Ellen Walton",
+		pos:"Vice Chairman",
+		face: "ri-facebook-circle-fill",
+		tweet:  "ri-twitter-fill",
+		linked: "ri-linkedin-box-fill"
+
+	},
+
+]
+
 export{
+	gridData,
     navLinks,
 }
