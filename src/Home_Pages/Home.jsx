2452a5784269fc5ee00ea85aade791b85a816eb0
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-05 03:19:52 +0530

    Home Layout Completed for now

--- src/Home_Pages/Home.jsx
+++ src/Home_Pages/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Hero from './Hero'
 import Layout from '../components/Layout'
+import Section2 from './Section2'
+import Section3 from './Section3'
+import Section4 from './Section4'
+import Section5 from './Section5'
+
 
 const Home = () => {
   return (
     <>
         <Layout>
             <Hero/>
+            <Section2/>
+            <Section3/>
+            <Section4/>
+            <Section5/>
+            
         </Layout>
     </>
   )
