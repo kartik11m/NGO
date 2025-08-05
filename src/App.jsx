11c04706a3319abc4ebf1899e78e54596903f215
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-05 17:15:04 +0530

    added route for volunteer form page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './Home_Pages/Home';
 import About from './About_Pages/About';
+import Vol from './Volunteer Page/Vol';
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import gsap from 'gsap';
 import { ScrollTrigger , SplitText } from 'gsap/all'
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
+        <Route path='/volunteer' element={<Vol/>}></Route>
       </Routes>
     </Router>
   )
