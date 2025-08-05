855a1c9e386d480d6aa02c9d2181c6adc5c976a4
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-05 17:38:57 +0530

    using GSAP for animating the h2 tag

--- src/Home_Pages/Hero.jsx
+++ src/Home_Pages/Hero.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
 
 const Hero = () => {
+  useGSAP(()=>{
+    const paragraphSplit = new SplitText('.subtitle' , {type : 'lines'}); // for animating the subtitle line by line
+    gsap.from(paragraphSplit.lines , {
+            opacity:0,
+            yPercent: 100,
+            duration:1.8,
+            ease:'expo.out',
+            stagger:0.06,
+        })
+  })
   return (
     <div id='hero' className='pt-20 max-md:pt-40'>
         <img src="/images/bg.png" alt=""  className='relative bg-center bg-cover lg:w-full'/>
         <div className='absolute top-[40%]  max-sm:mt-7 left-8'>
-            <h2 className='text-white font-bold max-sm:text-xl max-md:text-2xl max-lg:text-4xl lg:text-6xl xl:text-8xl'>Inclusive care for children <br />with special <br /> needs</h2>
+            <h2 className='subtitle text-white font-bold max-sm:text-xl max-md:text-2xl max-lg:text-4xl lg:text-6xl xl:text-8xl'>Inclusive care for children <br />with special <br /> needs</h2>
             <div className='flex mt-8'>
                 <button className=' bg-white rounded px-4 py-2 font-medium hover:bg-black hover:text-white md:text-2xl max-sm:px-2 max-sm:py-0'>What we do</button>
                 <button className='text-white ml-8 px-4 py-2 font-medium'><i className="ri-play-circle-line text-xl md:text-2xl">Play video</i> </button>
